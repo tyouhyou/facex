272edc83282bb47e0282aab951cd952ed635247b
tyouhyou <zb_116@hotmail.com>
2019-02-19 23:45:45 +0900

    use fer+

--- __main__.py
+++ __main__.py
@@ -1,5 +1,9 @@
 from facialexpress import FacialExpress as fe
+from ferplus import FerPlus as fp
 
 if __name__ == "__main__":
     # fe().make_model()
-    fe().predict(img_file=r'a.jpg')
+    # fe().predict(img_file=r'a.jpg')
+
+    fp().extract_imgs()
+    
